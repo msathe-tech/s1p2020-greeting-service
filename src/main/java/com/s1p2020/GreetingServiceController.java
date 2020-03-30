e5adccad0023827772f25c33d807236dec915f35
Madhav Sathe <msathe-tech@gmail.com>
2020-03-30 16:28:05 -0400

    Add the source code

---
+++ src/main/java/com/s1p2020/GreetingServiceController.java
@@ -0,0 +1,14 @@
+package com.s1p2020;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingServiceController {
+
+	@GetMapping("/greeting")
+	public String greeting() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/com/s1p2020/S1p2020GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1p2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S1p2020GreetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
