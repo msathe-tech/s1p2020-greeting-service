625cbdd49d8d2352b2f3c00eb2d4b33f50cf442e
Madhav Sathe <msathe.tech@gmail.com>
2021-02-16 05:41:06 +0000

    Update string

--- src/main/java/com/s1p2020/GreetingServiceController.java
+++ src/main/java/com/s1p2020/GreetingServiceController.java
@@ -10,10 +10,12 @@ public class GreetingServiceController {
 
 	@GetMapping("/greeting")
 	public String greeting() {
+        System.out.println("Invoked /greeting");
 		return "Greetings!";
 	}
 	@GetMapping("/")
 	public String home() {
+        System.out.println("Invoked /");
        /*try { 
             TimeUnit.SECONDS.sleep(1);        
         } catch (Exception e) {}
