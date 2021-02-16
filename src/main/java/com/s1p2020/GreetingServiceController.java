96c5edd25fd917eebfaf66502f47e353e437df4f
Madhav Sathe <msathe.tech@gmail.com>
2021-02-16 00:08:13 +0000

    Update string

--- src/main/java/com/s1p2020/GreetingServiceController.java
+++ src/main/java/com/s1p2020/GreetingServiceController.java
@@ -3,6 +3,7 @@ package com.s1p2020;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class GreetingServiceController {
@@ -13,6 +14,7 @@ public class GreetingServiceController {
 	}
 	@GetMapping("/")
 	public String home() {
+        TimeUnit.SECONDS.sleep(1);
 		return "Feb 15, 6.49pm Hello World!";
 	}
 }
