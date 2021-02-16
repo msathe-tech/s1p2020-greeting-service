37daa3f40b344270340a295db298d3ca2e0e9cb3
Madhav Sathe <msathe.tech@gmail.com>
2021-02-16 09:28:37 +0000

    Update string

--- src/main/java/com/s1p2020/GreetingServiceController.java
+++ src/main/java/com/s1p2020/GreetingServiceController.java
@@ -20,6 +20,6 @@ public class GreetingServiceController {
             TimeUnit.SECONDS.sleep(1);        
         } catch (Exception e) {}
        */
-		return "Feb 16, 3.46am Hello World!";
+		return "Feb 16, 4.28am Hello World!";
 	}
 }
